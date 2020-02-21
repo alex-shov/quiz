c489088cce493319548e5eca7ffb6a4e9648a321
alex-shov <46867417+alex-shov@users.noreply.github.com>
2020-02-22 00:33:29 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        text: 'Что не считается содержимым при box-sizing: border-box?',
+        options: ['padding', 'margin', 'border', 'Всё считается содерижмым'],
+        correct: 1
+    },
+    {
+        text: 'Каким будет результат вычисления 1 / 0 в JavaScript?',
+        options: ['null', 'undefined', 'Infinity', 'Ошибка'],
+        correct: 2        
+    },
+    {
+        text: 'Что из представленного не является примитивом в JavaScript?',
+        options: ['string', 'undefined', 'number', 'Object'],
+        correct: 3        
+    },
+    {
+        text: 'В каком стандарте определения цвета в CSS задаётся прозрачность?',
+        options: ['HTML-colors', 'RGB-палитра', 'RGBA', 'HEX-colors'],
+        correct: 2        
+    },
+    {
+        text: 'Что в структуре HTML-документа не должно попадать в head?',
+        options: ['Декларация &lt;!DOCTYPE html&gt;', 'Указание кодировки сайта', 'Подключение внешних файлов (css, js)', 'Добавление favicon'],
+        correct: 0
+    }    
+];  
